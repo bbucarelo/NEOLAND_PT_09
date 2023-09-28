1a39d849addccfc2761d7190d3a1b6998f0f1038
bbucarelo <b.bucarelo@gmail.com>
2023-09-28 20:59:17 +0200

    ejercicios menos reto

---
+++ JS_HTML_CSS/js_basic_algorithms/app_1.js
@@ -0,0 +1,6 @@
+let myFavoriteHero = "hulk";
+let x = 50; 
+let h = 5;
+let y = 10;
+let z = x + y; 
+console.log(z)
---
+++ JS_HTML_CSS/js_basic_algorithms/app_3.js
@@ -0,0 +1,18 @@
+mult = 10 * 5
+console.log(mult)
+
+div = 10 / 2
+console.log(div)
+
+rest = 15 % 9
+console.log(rest)
+
+const p = 10
+const j = 5
+const o = p + j
+console.log(o)
+
+const c = 10
+m = 5
+i = c * m
+console.log(i)
