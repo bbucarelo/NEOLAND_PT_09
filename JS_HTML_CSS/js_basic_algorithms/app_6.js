bf2e04a82e59e6f88c6596ec9439022c2dd2faf9
bbucarelo <b.bucarelo@gmail.com>
2023-10-06 19:35:54 +0200

    Mas bucles

--- JS_HTML_CSS/js_basic_algorithms/app_6.js
+++ JS_HTML_CSS/js_basic_algorithms/app_6.js
@@ -1,12 +1,16 @@
 for (i = 0; i <= 9; i++) 
 console.log(i)
 
-for (j = 0; j < 10; j++) {
+for (let j = 0; j <= 9; j++) {
 
 if (j % 2 === 0) 
 console.log(j)
 }
 
+for (let k = 0; k < 10; k++) {
 
-
-
+    if (k < 9) {
+    console.log("Intentando dormir")
+    }
+    else console.log("Dormido")
+}
