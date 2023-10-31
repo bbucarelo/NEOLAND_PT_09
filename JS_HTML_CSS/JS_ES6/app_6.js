e42fae8ec0d70813b17b1a269d87deabbe6b8399
bbucarelo <b.bucarelo@gmail.com>
2023-10-31 20:59:13 +0100

    + ES& 6.3

--- JS_HTML_CSS/JS_ES6/app_6.js
+++ JS_HTML_CSS/JS_ES6/app_6.js
@@ -24,3 +24,17 @@ const aliens = [
 	{name: 'Paktu', planet: 'Andromeda', age: 32},
 	{name: 'Cucushumushu', planet: 'Marte', age: 503021}
 ];
+
+const mutations = [
+	{name: 'Porompompero', description: 'Hace que el alien pueda adquirir la habilidad de tocar el tambor'},
+	{name: 'Fly me to the moon', description: 'Permite volar, solo y exclusivamente a la luna'},
+	{name: 'Andando que es gerundio', description: 'Invoca a un señor mayor como Personal Trainer'}
+];
+
+   const buscarAlien = aliens.find(alien => alien.name === "Cucushumushu");
+   const buscarMutacion = mutations.find(mutation => mutation.name === "Porompompero");
+   // console.log(buscarAlien);
+   // console.log(buscarMutacion); Solo quería ver que imprimia
+
+   const nuevoAlien = {...buscarAlien, ...buscarMutacion};
+   console.log("Su descripción es: ", nuevoAlien);
